996a3a03dfe861334675f87c6323061d88396418
Kartik Shrivastava <kartowl@Kartiks-MacBook-Pro.local>
2023-02-22 17:15:13 +0530

    refactor|test: refactor and add test for subscriber model

--- services/subscription/models.py
+++ services/subscription/models.py
@@ -1,6 +1,18 @@
 import uuid
 
-from django.db import models
+from django.db import models, DatabaseError
+
+
+class SubscriberManager(models.Manager):
+    def insert_subscriber(self, subscriber):
+        queryset = self.get_queryset()
+        try:
+            return queryset.create(endpoint_url=subscriber.endpoint_url,
+                                   expiration_time=subscriber.expiration_time,
+                                   p256dh_key=subscriber.p256dh_key,
+                                   auth_key=subscriber.auth_key)
+        except DatabaseError as e:
+            raise e
 
 
 class Subscriber(models.Model):
@@ -9,3 +21,8 @@ class Subscriber(models.Model):
     expiration_time = models.DateTimeField(null=True)
     p256dh_key = models.CharField(max_length=200)
     auth_key = models.CharField(max_length=200)
+
+    objects = SubscriberManager()
+
+    def __str__(self):
+        return f'{self.endpoint_url}'
