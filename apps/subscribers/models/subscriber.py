5a2c019f4b9454d2f1ac507caf448d96585decfd
Kartik Shrivastava <kartowl@Kartiks-MacBook-Pro.local>
2023-03-02 21:21:37 +0530

    fix: increase max_length of endpoint to support endpoint urls upto 500 chars

--- apps/subscribers/models/subscriber.py
+++ apps/subscribers/models/subscriber.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Subscriber(models.Model):
     subscriber_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    endpoint_url = models.CharField(unique=True, max_length=300)
+    endpoint_url = models.CharField(unique=True, max_length=500)
     expiration_time = models.DateTimeField(null=True)
     p256dh_key = models.CharField(max_length=200)
     auth_key = models.CharField(max_length=200)
