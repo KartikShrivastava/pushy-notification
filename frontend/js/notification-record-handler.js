e699bcb4dde17d225122e932320944662e554705
Kartik Shrivastava <kartowl@Kartiks-MacBook-Pro.local>
2023-03-06 16:16:17 +0530

    refact: remove unused code in notification-record-handler

--- frontend/js/notification-record-handler.js
+++ frontend/js/notification-record-handler.js
@@ -21,16 +21,6 @@ function triggerBulkPushNotification() {
     sendPayload.payload_id = selectedNotificationPayloadId
 
     console.log(JSON.stringify(sendPayload))
-
-    // return fetch('http://127.0.0.1:8000/notifications/records', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(payload),
-    // })
-    // .then(function (response) {
-    // })
 }
 
 document.getElementById('trigger-notifications')
