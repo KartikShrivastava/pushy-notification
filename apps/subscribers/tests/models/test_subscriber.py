cc8d1e3ca725e3daf080dda086684d265690bbe7
KartikShrivastava <shrivastavakartik19@gmail.com>
2023-03-09 09:41:04 +0530

    refact: move serializer usage in views to services for subscriber

--- apps/subscribers/tests/models/test_subscriber.py
+++ apps/subscribers/tests/models/test_subscriber.py
@@ -1,15 +1,19 @@
 from django.test import TestCase
 
+from apps.subscribers.models.subscriber import Subscriber
 from apps.subscribers.services.subscriber import SubscriberService
 
 
 class TestSubscriberModel(TestCase):
     def setUp(self):
-        self.validated_data = {'endpoint_url': 'https://fcm.googleapis.com/fcm/send/e-s0cN3uKzQ:APA91bF7FMhiKaweoRovSoz6bVYuDEFR2o4aCU9zCWjbRzoBWI1-KkV7M197JeyiZQuIm-Kg8St9UvHaElEg4lL028xA709bSeKLDrOYs2ghLKpKyWd4tZU6XGE0J5rbzq-xtxig0hsb',  # noqa: E501
-                               'expiration_time': None,
-                               'p256dh_key': 'BJthaLnJBkVtwe1SSAIbzSqm8A4vFw8P4SaF3yvmOW1IXzx4yUIvbmp4NVsuA5d0SBOVVUHfwYuak6CUJE1Qr9g',  # noqa: E501
-                               'auth_key': 'vDfqsQPFWabqpmeFFv6EEg'}
+        self.request_data = {'endpoint_url': 'https://fcm.googleapis.com/fcm/send/e-s0cN3uKzQ:APA91bF7FMhiKaweoRovSoz6bVYuDEFR2o4aCU9zCWjbRzoBWI1-KkV7M197JeyiZQuIm-Kg8St9UvHaElEg4lL028xA709bSeKLDrOYs2ghLKpKyWd4tZU6XGE0J5rbzq-xtxig0hsb',  # noqa: E501
+                             'expiration_time': None,
+                             'p256dh_key': 'BJthaLnJBkVtwe1SSAIbzSqm8A4vFw8P4SaF3yvmOW1IXzx4yUIvbmp4NVsuA5d0SBOVVUHfwYuak6CUJE1Qr9g',  # noqa: E501
+                             'auth_key': 'vDfqsQPFWabqpmeFFv6EEg'}
 
-    def test_string_representation_id(self):
-        subscriber = SubscriberService.create_subscriber(validated_data=self.validated_data)
-        self.assertEqual(str(subscriber), str(subscriber.subscriber_id))
+    def test_string_representation_returns_id(self):
+        serialized_subscriber = SubscriberService.create_subscriber(
+                                request_data=self.request_data)
+        subscriber = Subscriber.objects.get(
+                        subscriber_id=serialized_subscriber['subscriber_id'])
+        self.assertEqual(str(subscriber), serialized_subscriber['subscriber_id'])
--- apps/subscribers/tests/views/test_subscriber.py
+++ apps/subscribers/tests/views/test_subscriber.py
@@ -5,11 +5,11 @@ from apps.subscribers.services.subscriber import SubscriberService
 
 class TestSubscriberView(APITestCase):
     def setUp(self):
-        validated_data = {'endpoint_url': 'https://fcm.googleapis.com/fcm/send/e-s0cN3uKzQ:APA91bF7FMhiKaweoRovSoz6bVYuDEFR2o4aCU9zCWjbRzoBWI1-KkV7M197JeyiZQuIm-Kg8St9UvHaElEg4lL028xA709bSeKLDrOYs2ghLKpKyWd4tZU6XGE0J5rbzq-xtxig0hsb',  # noqa: E501
-                          'expiration_time': None,
-                          'p256dh_key': 'BJthaLnJBkVtwe1SSAIbzSqm8A4vFw8P4SaF3yvmOW1IXzx4yUIvbmp4NVsuA5d0SBOVVUHfwYuak6CUJE1Qr9g',  # noqa: E501
-                          'auth_key': 'vDfqsQPFWabqpmeFFv6EEg'}
-        self.subscriber = SubscriberService.create_subscriber(validated_data=validated_data)
+        request_data = {'endpoint_url': 'https://fcm.googleapis.com/fcm/send/e-s0cN3uKzQ:APA91bF7FMhiKaweoRovSoz6bVYuDEFR2o4aCU9zCWjbRzoBWI1-KkV7M197JeyiZQuIm-Kg8St9UvHaElEg4lL028xA709bSeKLDrOYs2ghLKpKyWd4tZU6XGE0J5rbzq-xtxig0hsb',  # noqa: E501
+                        'expiration_time': None,
+                        'p256dh_key': 'BJthaLnJBkVtwe1SSAIbzSqm8A4vFw8P4SaF3yvmOW1IXzx4yUIvbmp4NVsuA5d0SBOVVUHfwYuak6CUJE1Qr9g',  # noqa: E501
+                        'auth_key': 'vDfqsQPFWabqpmeFFv6EEg'}
+        self.subscriber = SubscriberService.create_subscriber(request_data=request_data)
 
     def tearDown(self):
         Subscriber.objects.all().delete()
